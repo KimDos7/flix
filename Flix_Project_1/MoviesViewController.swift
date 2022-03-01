45a51cc5fcbed81ca542e89e7b45f33f2e7c7d8f
Alex <younsuk.dory@gmail.com>
2022-02-28 19:26:43 -0500

    Part 2
    Updated readme along with part 2 of the flix project.

--- Flix_Project_1/MoviesViewController.swift
+++ Flix_Project_1/MoviesViewController.swift
@@ -40,7 +40,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                     // TODO: Reload your table  view data
                     self.tableView.reloadData()
                     print(dataDictionary)
-
              }
         }
         task.resume()
@@ -69,14 +68,24 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         return movies.count
     }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//         Get the new view controller using segue.destination.
+//         Pass the selected object to the new view controller.
+        print("Loading up the details screen")
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+
 
 }
